89dff2c23bc5813db2534a6457cf392f2de75fdf
fuyuezhen <976066889@qq.com>
2021-06-15 04:11:15 +0800

    update config

--- app/listener/HandsHakeListener.php
+++ app/listener/HandsHakeListener.php
@@ -62,7 +62,7 @@ class HandsHakeListener extends Listener
             //     'name'  => "client" . $time . $sid, // 用户名
             //     'service_url' => $url,
             // ],
-            $jwt = JWT::decode($token, $key, [$config->get('server.route.jwt.alg')]);
+            $jwt = JWT::decode($token, $key, $config->get('server.route.jwt.alg'));
             
             info($token);
             // 从jwt中获取信息
