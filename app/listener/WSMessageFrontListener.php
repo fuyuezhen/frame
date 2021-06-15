457f24363751a2a165c52b9248412e7ed3c5f7e7
fuyuezhen <976066889@qq.com>
2021-06-15 11:38:55 +0800

    update

--- app/listener/WSMessageFrontListener.php
+++ app/listener/WSMessageFrontListener.php
@@ -38,6 +38,7 @@ class WSMessageFrontListener extends Listener
          *  }
          */
         $data = json_decode($frame->data, true);
+        var_dump($data);
         $this->{$data['method']}($swoStarServer, $swooleServer, $data, $frame->fd);
     }
 
